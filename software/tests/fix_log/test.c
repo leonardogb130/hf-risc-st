e525a2408b7f626e3f887f59cc59c4df8ef12455
leonardogb130 <42787996+leonardogb130@users.noreply.github.com>
2019-10-04 02:31:47 -0300

    Update test.c

--- software/tests/fix_log/test.c
+++ software/tests/fix_log/test.c
@@ -5,6 +5,15 @@
 // função testada sera a fix_logaritmo
 fixed_t fix_log(fixed_t fp, fixed_t base);
 
+// funcao para comparação
+void assert(int test, const char * message){
+	if (test == 0) {
+		printf("Error: %s\n", message);
+		failed_tests++;
+	}
+	executed_tests++;
+}
+
 // Lista de testes 
 /* CLASSES DE EQUIVALENCIA
    Entrada 		 Classes Válidas 			 Classes Inválidas
@@ -28,86 +37,37 @@ fixed_t fix_log(fixed_t fp, fixed_t base);
 // testes
 void teste1();
 void teste2();
-void teste3();
-void teste4();
-//void teste5();
-
 
 // main test
 void hfunit_run_tests(){
 	teste1();
 	teste2();
-	teste3();
-	teste4();
-	//teste5();
 }
 
-// testa se funciona com um valor inteiro positivo, normal e com fix, deve funcionar
+// testa se funciona com um valor inteiro positivo, com fix, deve funcionar
 void teste1(){
-	
-	float result;
-	fixed_t result2;
-	
-	testa_hfunit_log(5, "Teste passou");
-	result = log(5);
-	result2 = fix_log(5,5);
- 
-	printf("Valor do log: %f\n", result);
-	printf("Valor do fix_log: %f\n", result2);
+
+        // setando o valor em fixed
+        fixed_t input = fix_val(5.5f);
+	// atribuicao do valor fixed a funcao fix_ln
+	fixed_t result = fix_log(input,10);
+
+	int8_t buf[30];
+	fixtoa(result, buf, 6);
+
+	assert(buf[0] == '1' && buf[1] == '.' && buf[2] == '7' && buf[3] == '0', "Log de 1.5f deve ser aproximadamente 1.704748");
 }
 
-// testa se funciona com valor = 0 ( deve falhar )
+
+
+ testa limite com fix
 void teste2(){
 	
-	float result;
-	fixed_t result2;
-	
-	testa_hfunit_log(0, "Teste falhou");
-	result = log(0);
-	result2 = fix_log(0,0);
- 
-	printf("Valor do log: %f\n", result);
-	printf("Valor do fix_log: %f\n", result2);
+	 // setando o valor em fixed
+        fixed_t input = fix_val(472700090348091277312);
+	// atribuicao do valor fixed a funcao fix_log
+	fixed_t result = fix_log(input,10);
 }
-
-// testa se funciona com valor = -3 ( deve falhar )
-void teste3(){
-	float result;
-	fixed_t result2;
-	
-	testa_hfunit_log(-3, "Teste falhou");
-	result = log(-3);
-	result2 = fix_log(-3,-3);
- 
-	printf("Valor do log: %f\n", result);
-	printf("Valor do fix_log: %f\n", result2);
-}
-
-// testa se funciona letra = Z ( deve falhar )
-void teste4(){
-	float result;
-	fixed_t result2;
-	
-	testa_hfunit_log('Z', "Teste falhou");
-	result = log('Z');
-	result2 = fix_log('Z','Z');
- 
-	printf("Valor do log: %f\n", result);
-	printf("Valor do fix_log: %f\n", result2);
-}
-
-// testa limite com e sem fix, deve acusar erro
-/*void teste5(){
-	float result;
-	fixed_t result2;
-	
-	testa_hfunit_log(472700090348091277312‬, "Teste falhou");
-	result = log(472700090348091277312‬);
-	result2 = fix_log(‭472700090348091277312‬,‭472700090348091277312‬);
- 
-	printf("Valor do log: %f\n", result);
-	printf("Valor do fix_log: %f\n", result2);
-}*/
 
 
 
