c824bdc26963e61d3db85905781414ff4da673ea
leonardogb130 <42787996+leonardogb130@users.noreply.github.com>
2019-10-04 17:07:57 -0300

    Add files via upload

--- software/tests/math/test.c
+++ software/tests/math/test.c
@@ -1,13 +1,17 @@
 #include <hf-risc.h>
 #include <hf-unit.h>
 
+extern int failed_tests;
+extern int executed_tests;
+
+
 // função testada sera a logaritmo
 float log(float arg);
 
 // funcao para teste
 void assert(int test, const char * message){
 	if (test == 0) {
-		printf("Error: %s\n", message);
+		printf("Erro: %s\n", message);
 		failed_tests++;
 	}
 	executed_tests++;
@@ -50,14 +54,15 @@ void hfunit_run_tests(){
 	teste5();
 }
 
-// testa se funciona com um valor inteiro positivo
+// testa se funciona com um valor inteiro positivo, funciona
 void teste1(){
 	
 	float resultado = log(5.5);
 	float expected = 1.704748;
 	printf("%f\n", resultado);
 
-	assert(result != expected, "Entrada inválida");
+	// resultado esperado igual ao resultado, então assert nao dispara
+	assert(resultado == expected, "Entrada válida");
  
 }
 
@@ -65,22 +70,24 @@ void teste1(){
 void teste2(){
 	
 	float resultado = log(0);
-	float -inf = -1.0/0.0;
-	float expected = -inf;
+	// deve gerar como resultado esperado inf
+	float inf = ((-1.0)/(0.0));
+	//atribuo o inf ao esperado
+	float expected = inf;
 	printf("%f\n", resultado);
-
-	assert(result != expected, "Entrada inválida");
+    // comparo o esperado com o resultado
+	assert(resultado == expected, "Impossível log(0)");
  
 }
 
 // testa se funciona com valor = -3 ( deve falhar )
 void teste3(){
-	float resultado = log(-3);
-	float NAN = 0.0/0.0;
-	float expected = NAN;
+	float resultado = log(-3.0);
+	float nan = ((0.0)/(0.0));
+	float expected = nan;
 	printf("%f\n", resultado);
 
-	assert(result != expected, "Entrada inválida");
+	assert(resultado == expected, "Impossível log(-3)");
  
 }
 
@@ -88,10 +95,11 @@ void teste3(){
 void teste4(){
 		
 	float resultado = log('z');
-	float expected = 4.682131;
+	float nan = ((0.0)/(0.0));
+	float expected = nan;
 	printf("%f\n", resultado);
 
-	assert(result != expected, "Entrada inválida");
+	assert(resultado == expected, "Entrada inválida");
  
 }
 
@@ -99,10 +107,10 @@ void teste4(){
 void teste5(){
 	
 	float resultado = log(472700090348091277312);
-	float expected = 43.891613;
+	float expected = 43891613;
 	printf("%f\n", resultado);
 
-	assert(result != expected, "Excedido limite");
+	assert(resultado > expected, "Excedido limite");
  
 }
 
